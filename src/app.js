33f8f1a9f75aec91d117120b9173206ef88f1904
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-28 11:51:39 -0700

    Added header component

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Header from './components/header/header.jsx';
+
+export default function App() {
+
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+
+    );
+
+};
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,1 +1,65 @@
-'use strict';
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+
+        flexGrow: 1,
+
+    },
+
+    menuButton: {
+
+        marginRight: theme.spacing(2),
+
+    },
+
+
+    title: {
+
+        flexGrow: 1,
+
+    },
+
+}));
+
+const styles = {
+
+    cart: {
+        'marginLeft': '5px'
+    }
+
+}
+
+
+export default function Header() {
+
+    const classes = useStyles();
+
+    return (
+        <header>
+            <AppBar position='static'>
+                <Toolbar>
+                    <Typography variant='h3' className={classes.title}>
+                        Virtual Store
+                    </Typography>
+                    <Button color='inherit'>
+                        Cart (0)
+                        <ShoppingCartIcon style={styles.cart} />
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </header>
+    )
+}
