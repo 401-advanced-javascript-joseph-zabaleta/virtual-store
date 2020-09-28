af98a5ea4ca1285e74bb2a68272ad03e12a4be6f
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-28 12:37:44 -0700

    Added a footer component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 import Header from './components/header/header.jsx';
+import Footer from './components/footer/footer.jsx';
 
 export default function App() {
 
     return (
         <React.Fragment>
             <Header />
+            <body>
+                <h1>
+                    Filler Body
+                </h1>
+            </body>
+            <Footer />
         </React.Fragment>
 
     );
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,1 +1,42 @@
-'use strict';
+import React from 'react'
+
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+const links = {
+    github: 'https://github.com/joseph-zabaleta',
+    linkedIn: 'https://www.linkedin.com/in/joseph-zabaleta/',
+};
+
+const styles = {
+
+    footer: {
+
+        'display': 'flex',
+        'flex-direction': 'column',
+        'align-items': 'center'
+
+    }
+
+};
+
+export default function Footer() {
+    return (
+        <footer style={styles.footer}>
+            <Typography>
+                &copy;Joseph Zabaleta
+            </Typography>
+            <Typography>
+                <Link href={links.github} color='inherit' rel='noopener' target='_blank'>
+                    <GitHubIcon fontSize='large' />
+                </Link>
+                <Link href={links.linkedIn} color='inherit' color='inherit' rel='noopener' target='_blank'>
+                    <LinkedInIcon fontSize='large' />
+                </Link>
+            </Typography>
+        </footer>
+    )
+};
