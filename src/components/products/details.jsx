1413c25e86d1cb0e950ea723d0a02a5e359a1270
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-30 16:10:46 -0700

    added more styling to details page

--- src/components/products/details.jsx
+++ src/components/products/details.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -11,17 +11,55 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
-const useStyles = makeStyles({
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
+
+
+const useStyles = makeStyles((theme) => ({
+
     root: {
         margin: '10px',
         padding: '15px',
-        maxWidth: '600px'
+        width: '100%'
     },
     media: {
         height: 400,
     },
-});
+
+    container: {
+        width: 600,
+        display: 'grid',
+        flexDirection: 'column',
+        alignItems: 'center'
+
+    },
+
+    list: {
+
+        width: '100%',
+
+    },
+
+    nested: {
+
+        paddingLeft: theme.spacing(4)
+
+    }
+
+}));
 
 const styles = {
 
@@ -38,12 +76,45 @@ const styles = {
 
     },
 
+    content: {
+
+        padding: 0,
+        display: 'grid',
+        alignItems: 'flex-start'
+
+    },
+
     quantity: {
 
         flexGrow: 1,
-        justify: 'flex-start'
+        textAlign: 'left',
 
+    },
 
+    price: {
+
+        flexGrow: 1,
+        textAlign: 'right'
+
+    },
+
+    spanTitle: {
+
+        color: 'grey',
+        paddingRight: 5
+
+    },
+
+    spanContent: {
+
+        fontWeight: 500,
+
+    },
+
+    button: {
+
+        width: '100%',
+        marginLeft: 10,
 
     }
 }
@@ -51,10 +122,17 @@ const styles = {
 function Details(props) {
 
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+
+        setOpen(!open);
+
+    };
 
     return (
         <React.Fragment>
-            <Container align='center'>
+            <Container className={classes.container} align='center'>
                 <Typography
                     variant='h2'
                     align='center'>
@@ -72,22 +150,66 @@ function Details(props) {
                         title={props.product.name}
                         alt={props.product.name}
                         image='https://via.placeholder.com/600x400' />
-                    <CardContent className={styles.content}>
+                    <CardContent style={styles.content}>
                         {/* <Toolbar>
                             <div>hello</div>
                             <div>HI</div>
                         </Toolbar> */}
                         <Toolbar>
                             <Typography style={styles.quantity}>
-                                Hello
-                        </Typography>
-                            <Typography>
-                                Bye
-                        </Typography>
+                                <span style={styles.spanTitle}>
+                                    Quantity:
+                                </span>
+                                <span style={styles.spanContent}>
+                                    {props.product.count}
+                                </span>
+                            </Typography>
+                            <Typography style={styles.price}>
+                                <span style={styles.spanContent}>
+                                    ${props.product.price}
+                                </span>
+                            </Typography>
                         </Toolbar>
 
                     </CardContent>
                 </Card>
+
+                <Button
+                    variant='contained'
+                    color='primary'
+                    style={styles.button}>
+                    Buy
+                </Button>
+
+                <br />
+                <Typography
+                    variant='h5' align='left'>
+                    Product Details
+                </Typography>
+
+
+                <List
+                    component="nav"
+                    className={classes.list}>
+                    <Paper square>
+                        <ListItem button onClick={handleClick}>
+                            <ListItemText primary="Specifications" />
+                            {open ? <ExpandLess /> : <ExpandMore />}
+                        </ListItem>
+
+                    </Paper>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Paper square>
+                            <List component="div" disablePadding>
+                                <ListItem button className={classes.nested}>
+                                    <ListItemText primary="Product Specs go here" />
+                                </ListItem>
+                            </List>
+
+                        </Paper>
+                    </Collapse>
+                </List>
+
             </Container >
         </React.Fragment>
     )
